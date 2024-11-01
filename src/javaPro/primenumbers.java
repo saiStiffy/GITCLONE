1a58e5864135b0f069c5cf1b8fff5647f2ae23df
saiStiffy <saikrn1997@gmail.com>
2024-11-01 14:16:42 +0530

    first chnages from local to github

---
+++ src/javaPro/primenumbers.java
@@ -0,0 +1,24 @@
+package javaPro;
+
+import java.util.Scanner;
+
+public class primenumbers {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("eneter a number");
+	int num=sc.nextInt();
+	if(num%2==0) {
+		System.out.println("its even number");
+		
+	}else {
+		System.out.println("not even number");
+	}
+System.out.println("changed made");
+System.out.println("from demo to git");
+System.out.println("from develop branceh changes ");
+
+System.out.println("from develop branceh changes to another");
+	}
+
+}
